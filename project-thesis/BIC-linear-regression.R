84b4627ad5093ff62836750721cd997d02bf6477
jonasschwery <jonas@schwery.ch>
2019-12-29 18:37:21 +0100

    finish first version with BIC-weighting in linear regression

--- project-thesis/BIC-linear-regression.R
+++ project-thesis/BIC-linear-regression.R
@@ -1,4 +1,4 @@
-linRegBIC <- function(X, y, candidate_models, likelihood) {
+linRegBIC <- function(X, y, candidate_models) {
   
   s_k <- apply(candidate_models, 2, sum);
   k <- ncol(candidate_models);
@@ -7,6 +7,7 @@ linRegBIC <- function(X, y, candidate_models, likelihood) {
   p <- ncol(X);
   w_k_numerator <- C_k <- numeric(k);
   phi <- 1;
+  
   for (i in 1:k) {
     
     # get the indices of the model to select the right positions of the design matrix
@@ -14,17 +15,31 @@ linRegBIC <- function(X, y, candidate_models, likelihood) {
     
     # extract the right indices form the candidate model in the current iteration
     # out of the design matrix
-    Xs_k <- X[,indices];
+    Xs_k <- X[ ,indices];
     
-    l_k <- 0; ## maximized likelihood of model k
-    I_k[i] <- -2*log(lk) + sk[i] * log(n); ## definition of I_k_BIC
+    # prepare the data for the linear regression
+    reg_data <- as.data.frame(cbind(y, Xs_k));
+    colnames(reg_data)[1] <- "y";
+    
+    # fit standard linear regression of y on Xs_k using the training set D1
+    fit_lm_k <- lm( y ~ . , reg_data );
+    
+    
+    # calculate the residual sum of squares, defined as t(y)%*%y + t(y)%*% X %*% solve(t(X)%*% X) %*% t(X)%*% y
+    # for more infos, check: https://en.wikipedia.org/wiki/Residual_sum_of_squares
+    residualSumOfSquares <- sum(resid(fit_lm_k)^2);
+    
+    # definition of I_k_BIC, see 'Gaussian special case' in https://en.wikipedia.org/wiki/Bayesian_information_criterion
+    I_k[i] <- n*log(residualSumOfSquares / n) + p*log(n);
     C_k[i] <- s_k[i] * log( exp(1) * p / s_k[i] ) + 2 * log(s_k[i] + 2);
-    w_k_numerator[i] <- exp(-I_k[i] / 2 - phi * C_k);
+    w_k_numerator[i] <- exp(-I_k[i] / 2 - phi * C_k[i]);
     
   }
-  w_k_denominator <- sum( exp(I_k/2 - phi*C_k) );
+  
+  w_k_denominator <- sum( exp(-I_k/2 - phi*C_k) );
   weight_vector <- w_k_numerator / w_k_denominator;
   soil_importance <- weight_vector %*% t(candidate_models);
+  
   return (
     list(
       weight_vector = round(weight_vector, 2), 
--- project-thesis/test-soil-algorithm.R
+++ project-thesis/test-soil-algorithm.R
@@ -1,6 +1,7 @@
 source("generate-data.R");
 source("candidate-models.R");
 source("ARM-linear-regression.R");
+source("BIC-linear-regression.R");
 
 #### Generating data ####
 # number of observations
@@ -9,21 +10,21 @@ n <- 100;
 p <- 5;
 rho <- 0.9;
 sd <- 0.1;
-beta_star <- matrix(c(2,1,0,0,0), ncol = 1);
+beta_star <- matrix(c(0,0,1,0,0), ncol = 1);
 data <- generateData(n = n, p = p, rho = rho, sd = sd, beta_star = beta_star);
 X <- data$X;
 y <- data$y;
 
 #### Generating the candidate models ####
-L = 100 # number of different regularization parameters
+L = 1000 # number of different regularization parameters
 candidate_models <- candidateModels(X = X, y = y, L = L);
 print(candidate_models);
 
 #### Weighting using ARM with non uniform priors ####
 nsim = 1000;
 linRegARM <- linRegARM(X = X, y = y, nsim = nsim, candidate_models = candidate_models);
-linRegARM$weight_vector;
-linRegARM$soil_importance;
+linRegARM
+
 
 # Compare with the implementation from the autor, see example from official package manual
 library(SOIL);
@@ -32,10 +33,14 @@ v_ARM
 
 
 #### Weighting using information criteria with nonuniform priors ####
+linRegBIC <- linRegBIC(X = X, y = y, candidate_models = candidate_models);
+print(candidate_models)
+linRegBIC
 
 
-
-
+library(SOIL);
+v_ARM <- SOIL(X, y, family = "gaussian", weight_type = "BIC", prior = TRUE);
+v_ARM
 
 
 
